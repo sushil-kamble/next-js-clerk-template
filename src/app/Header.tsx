c1ce1539c86a9f8a30553a92a13c6263dc24896b
Sushil Kamble <iamsushil303@gmail.com>
2025-03-23 15:55:58 +0530

    Implement error and not found pages with custom SVG illustrations and improved header authentication handling

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,15 +1,35 @@
-import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  UserButton,
+  SignInButton,
+  SignedOut,
+  SignedIn,
+} from "@clerk/nextjs";
 import Link from "next/link";
 
-export function Header() {
+export async function Header() {
   return (
-    <header className="border-b">
-      <div className="mx-auto container py-4">
-        <div className="flex items-center justify-between">
+    <header className="border-b h-16">
+      <div className="mx-auto container h-full">
+        <div className="flex items-center justify-between h-full">
           <Link href="/" className="text-xl font-bold">
             Next.js + Clerk
           </Link>
-          <UserButton />
+          <ClerkLoading>
+            <div className="w-20 h-6 bg-gray-200 rounded animate-pulse"></div>
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignInButton mode="modal">
+                <Button>Login</Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </ClerkLoaded>
         </div>
       </div>
     </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 async function Page() {
   return (
-    <div>
-      <div className="mb-8 flex items-center justify-between">
+    <div className="my-4">
+      <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold">Next.js + Clerk with Tailwind v4</h1>
       </div>
     </div>
